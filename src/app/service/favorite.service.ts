ae5607b41adc5bdba4a323aa48342a4bec4e3aa9
Carina Nuage <carinavbritto@nuageit.com.br>
2024-06-19 15:53:55 -0300

    feat: add favorites list persist

--- src/app/service/favorite.service.ts
+++ src/app/service/favorite.service.ts
@@ -6,8 +6,11 @@ import { Character } from '../interfaces/character.interface';
 })
 export class FavoriteService {
   private favorites: Character[] = [];
+  private storageKey = 'favoriteCharacters';
 
-  constructor() {}
+  constructor() {
+    this.loadFavorites();
+  }
 
   getFavorites(): Character[] {
     return this.favorites;
@@ -17,6 +20,7 @@ export class FavoriteService {
     const index = this.favorites.findIndex((fav) => fav.id === character.id);
     if (index === -1) {
       this.favorites.push(character);
+      this.saveFavorites();
     }
   }
 
@@ -24,6 +28,7 @@ export class FavoriteService {
     const index = this.favorites.findIndex((fav) => fav.id === character.id);
     if (index !== -1) {
       this.favorites.splice(index, 1);
+      this.saveFavorites();
     }
   }
 
@@ -33,5 +38,16 @@ export class FavoriteService {
 
   isFavorite(character: Character): boolean {
     return this.favorites.some((fav) => fav.id === character.id);
+  }
+
+  private saveFavorites(): void {
+    localStorage.setItem(this.storageKey, JSON.stringify(this.favorites));
+  }
+
+  private loadFavorites(): void {
+    const favorites = localStorage.getItem(this.storageKey);
+    if (favorites) {
+      this.favorites = JSON.parse(favorites);
+    }
   }
 }
